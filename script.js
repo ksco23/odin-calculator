80ef32fd60e58e0774527e5d7bc1c0d78df37167
Kyle Scoggin <243018999+ksco23@users.noreply.github.com>
2026-01-03 16:08:28 -0600

    Create the operate function
    The operate function takes two numbers and an operator and decides
    which math operation function to call based on the operator.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+let numA = 0;
+let numB = 0;
+let op = '';
+
+console.log(operate(2, 2, '+'));
+console.log(operate(4, 2, '-'));
+console.log(operate(3, 2, '*'));
+console.log(operate(8, 2, '/'));
+console.log(operate(2, 2, 'x'));
 
 
+
+function operate(a, b, op){
+    if(op === '+'){
+        return add(a, b);
+    }
+    else if(op === '-'){
+        return subtract(a, b);
+    }
+    else if(op === '*'){
+        return multiply(a, b);
+    }
+    else if(op === '/'){
+        return divide(a, b);
+    }
+    else{
+        console.error('In operate, op is not valid.');
+    }
+}
 
 function add(a,b){
     return a + b;
