7cfa5e69a514637c18fe5dcae51f608d788358c2
Kyle Scoggin <243018999+ksco23@users.noreply.github.com>
2026-01-03 18:49:41 -0600

    Add functions validate and display input
    Validating input on numerical input includes not allowing multiple
    leading zeros, not allowing multiple decimals, and not accepting
    more digits than will fit in the display area.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const MAX_DISPLAY_LENGTH = 13;
+
+const btnContainer = document.querySelector('#buttons');
+const calcDisplay = document.querySelector('#display');
+
 let numA = 0;
 let numB = 0;
 let op = '';
 
-/*console.log(operate(2, 2, '+'));
-console.log(operate(4, 2, '-'));
-console.log(operate(3, 2, '*'));
-console.log(operate(8, 2, '/'));
-console.log(operate(2, 2, 'x'));*/
 
 
+btnContainer.addEventListener('click', btnClickEvt);
+
+function btnClickEvt(e){
+    if(e.target.tagName === 'BUTTON'){
+        if(e.target.id === 'clearBtn'){
+            clear();
+        }
+        else{
+            validateInput(e.target.textContent);
+        }
+    }
+}
 
 function operate(a, b, op){
     if(op === '+'){
@@ -39,4 +51,45 @@ function multiply(a,b){
 }
 function divide(a,b){
     return a / b;
+}
+
+function clear(){
+    numA = 0;
+    numB = 0;
+    op = '';
+    updateDisplay('0');
+}
+
+function validateInput(inputTxt){
+    //get current display
+    //IF the input is a number or decimal
+        //IF input is 0 only don't accpet it if current display is 0
+        //IF input is a decimal don't accpt it if current display already contains a decimal
+        
+    const curDisplay = calcDisplay.textContent;
+
+    if(curDisplay.length < MAX_DISPLAY_LENGTH){
+        if(inputTxt.search(/[1-9]/) !== -1){
+            if(curDisplay === '0'){
+                updateDisplay(inputTxt);
+            }
+            else{
+                updateDisplay(curDisplay + inputTxt);
+            }
+        }
+        else if(inputTxt === '0'){
+            if(curDisplay !== '0'){
+                updateDisplay(curDisplay + inputTxt);
+            }
+        }
+        else if(inputTxt === '.'){
+            if(!curDisplay.includes('.')){
+                updateDisplay(curDisplay + inputTxt);
+            }
+        }
+    }
+}
+
+function updateDisplay(displayString){
+    calcDisplay.textContent = displayString;
 }
